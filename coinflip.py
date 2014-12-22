72f8984eff228b611ee3173cb95ef03929baa416
Tim Jabaut <tjabaut@gmail.com>
2014-12-22 08:48:05 -0500

    Added a function for the CoinFlip

--- coinflip.py
+++ coinflip.py
@@ -2,12 +2,14 @@ __author__ = 'tjabaut'
 
 import random
 
-coinflip = random.randint(0, 1)
+
+def coinflip():
+    outcome = random.randint(0, 1)
+    return outcome
 
 # print coinflip
 
-if coinflip == 0:
+if coinflip() == 0:
     print "Heads"
-
 else:
     print "Tails"
